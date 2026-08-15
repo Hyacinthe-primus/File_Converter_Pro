f929150793f00dc64a97fb60cdc05cd35f5252a1
Hyacinthe-primus <hyacintheatho91@gmail.com>
2026-08-15 03:33:01 +0100

    chore: formatting

--- dialogs/__init__.py
+++ dialogs/__init__.py
@@ -6,20 +6,20 @@ Re-exports public classes so that app.py can continue to write:
 
 """
 
-from .pdf_to_word_dialog import PdfToWordDialog
-from .splash_screen import ModernSplashScreen
-from .preview_dialog import PreviewDialog
-from .password_dialog import PasswordDialog
-from .split_dialog import SplitDialog
-from .compression_dialog import CompressionDialog
 from .batch_convert_dialog import BatchConvertDialog
 from .batch_rename_dialog import BatchRenameDialog
-from .settings_dialog import SettingsDialog
+from .compression_dialog import CompressionDialog
 from .conversion_options_dialog import ConversionOptionsDialog
+from .merge_order_dialog import MergeOrderDialog
+from .password_dialog import PasswordDialog
+from .pdf_protection_dialog import PdfProtectionDialog
+from .pdf_to_word_dialog import PdfToWordDialog
+from .preview_dialog import PreviewDialog
+from .settings_dialog import SettingsDialog
+from .splash_screen import ModernSplashScreen
+from .split_dialog import SplitDialog
 from .terms_dialog import TermsAndPrivacyDialog
 from .word_to_pdf_dialog import WordToPdfOptionsDialog
-from .pdf_protection_dialog import PdfProtectionDialog
-from .merge_order_dialog import MergeOrderDialog
 
 __all__ = [
     "PdfToWordDialog",
