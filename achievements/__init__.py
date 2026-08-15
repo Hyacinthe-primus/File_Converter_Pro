4d78d66baf79257d328614c7d5c07ea6405e0581
Hyacinthe-primus <hyacintheatho91@gmail.com>
2026-08-15 03:32:48 +0100

    chore: formatting

--- achievements/__init__.py
+++ achievements/__init__.py
@@ -6,10 +6,10 @@ Re-exports public classes so that app.py can continue to write:
 
 """
 
-from .achievements_system  import AchievementSystem
-from .achievements_ui       import AchievementsUI
-from .achievements_popup    import AchievementPopup
-from .rank_popup            import RankPopup
+from .achievements_popup import AchievementPopup
+from .achievements_system import AchievementSystem
+from .achievements_ui import AchievementsUI
+from .rank_popup import RankPopup
 
 __all__ = [
     "AchievementSystem",
